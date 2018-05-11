03d6a7911f3eb93cf467dc30ed09273d5ba64e04
Alejandra Rubalcava <arubalcava@group.on>
2018-05-11 14:51:05 -0500

    matching logic added

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,5 @@
+import { parse } from 'url';
+
 //this page determines what data the user sees and what the user is able to post to store in server
 //require all of the data needed
 let friendsData = require('../data/friends');
@@ -8,8 +10,32 @@ module.exports = function (app) {
         res.json(friendsData);
     });
 
-    app.post('/api/tables', function (req, res) {
-        friendsData.push(req.body);
-        res.json(true);
+    app.post('/api/friends', function (req, res) {
+        //create variables for info user entered into survey
+        let userData = req.body;
+        let userScores = userData.scores;
+        //create variables to find furry friend match
+        let scoresArr = [];
+        let match = 0;
+           
+
+        for (let i = 0; i < friendsData.length; i++) {
+            let compDiff = 0;
+            for (let j = 0; j < userScores.length; j++) {
+                compDiff += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(userScores[j])))
+            }
+            scoresArr.push(compDiff);
+        }
+        //loop through scoreArr to determine best match 
+        for (let k = 0; k < scoresArr.length; k++) {
+            if(scoresArr[k] <= scoresArr[match]) {
+                match = k;
+            }
+        }
+        let bestFriend = friendsData[match];
+        res.json(bestFriend);
+
+        friendsData.push(userData);
+        
     });
-}
+};
